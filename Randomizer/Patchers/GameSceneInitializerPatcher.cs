7503978d36db2490afc3a09747d7937e6e0f2871
SloopyNoSleep <131174641+SloopyNoSleep@users.noreply.github.com>
2023-05-10 09:35:36 +0200

    Add POI permutation

---
+++ Randomizer/Patchers/GameSceneInitializerPatcher.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using Randomizer.Helpers;
+using UnityEngine;
+using Winch.Core;
+
+namespace Randomizer.Patchers;
+
+[HarmonyPatch(typeof(GameSceneInitializer))]
+[HarmonyPatch("Start")]
+public class GameSceneInitializerPatcher
+{
+    public static void Prefix(GameSceneInitializer __instance)
+    {
+        try
+        {
+            var harvestPoisContainer = GameObject.Find("HarvestPOIs");
+            if (harvestPoisContainer == null)
+            {
+                WinchCore.Log.Error("HarvestPOIs not found");
+                return;
+            }
+
+            var poiList = PoiHelpers.GetPoiListFromContainer(harvestPoisContainer);
+            PoiHelpers.PermuteHarvestPoiLocations(poiList);
+        }
+        catch (Exception ex)
+        {
+            WinchCore.Log.Error($"Error in {nameof(GameSceneInitializerPatcher)}: exception {ex}");
+        }
+    }
+}
