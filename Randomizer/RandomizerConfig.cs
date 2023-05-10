836a3bf4de0cc0a2a6666cbbb505ffaa47810901
SloopyNoSleep <131174641+SloopyNoSleep@users.noreply.github.com>
2023-05-10 09:32:28 +0200

    Add initialization logic from ModConfig to RandomizerConfig

--- Randomizer/RandomizerConfig.cs
+++ Randomizer/RandomizerConfig.cs
@@ -1,7 +1,14 @@
-﻿namespace Randomizer;
+﻿using System;
+using Winch.Config;
+
+namespace Randomizer;
 
 public class RandomizerConfig
 {
+    private static RandomizerConfig? _instance;
+
+    public static RandomizerConfig Instance => _instance ??= new();
+
     public bool RandomizeHarvestPoIs { get; set; } = true;
     public bool RandomizePoiCoordinates { get; set; } = true;
     public bool RandomizeSizes { get; set; } = true;
@@ -9,5 +16,18 @@ public class RandomizerConfig
     public bool RandomizeHarvestMinigamesTypes { get; set; } = true;
     public bool RandomizeHarvestableType { get; set; } = true;
     public bool RandomizeDifficulty { get; set; } = true;
-    public Int32? Seed { get; set; } = null;
+    public int? Seed { get; set; } = null;
+
+    public static void Initialize()
+    {
+        Instance.RandomizeDifficulty = ModConfig.GetProperty("Randomizer", "RandomizeDifficulty", true);
+        Instance.RandomizeHarvestPoIs = ModConfig.GetProperty("Randomizer", "RandomizeHarvestPoIs", true);
+        Instance.RandomizePoiCoordinates = ModConfig.GetProperty("Randomizer", "RandomizePoiCoordinates", true);
+        Instance.RandomizeSizes = ModConfig.GetProperty("Randomizer", "RandomizeSizes", true);
+        Instance.RandomizeQuestsRequirements = ModConfig.GetProperty("Randomizer", "RandomizeQuestsRequirements", true);
+        Instance.RandomizeHarvestMinigamesTypes = ModConfig.GetProperty("Randomizer", "RandomizeHarvestMinigamesTypes", true);
+        Instance.RandomizeHarvestableType = ModConfig.GetProperty("Randomizer", "RandomizeHarvestableType", false);
+        Instance.RandomizeDifficulty = ModConfig.GetProperty("Randomizer", "RandomizeDifficulty", true);
+        Instance.Seed = ModConfig.GetProperty("Randomizer", "Seed", SeededRng.Seed);
+    }
 }
