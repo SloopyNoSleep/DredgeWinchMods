457ee89ea11fe4c1bb802fdcfa1be69926395715
SloopyNoSleep <131174641+SloopyNoSleep@users.noreply.github.com>
2023-05-10 09:31:32 +0200

    Add FisherYatesShuffle for permutations

--- Randomizer/SeededRng.cs
+++ Randomizer/SeededRng.cs
@@ -1,15 +1,27 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace Randomizer;
+﻿namespace Randomizer;
 
 public static class SeededRng
 {
-    public static Int32 Seed = Guid.NewGuid().GetHashCode();
+    public static int Seed = Guid.NewGuid().GetHashCode();
 
     private static Random? _rng;
     public static Random? Rng
     {
         get => _rng ??= new(Seed);
-        private set => _rng = value;
+    }
+
+    public static List<T> FisherYatesShuffle<T>(List<T> originalList)
+    {
+        List<T> list = new(originalList);
+        if (Rng == null) return originalList;
+
+        var n = list.Count();
+        for (int i = 0; i < (n - 1); i++)
+        {
+            var swapIdx = i + SeededRng.Rng.Next(n - i);
+            (list[i], list[swapIdx]) = (list[swapIdx], list[i]);
+        }
+
+        return list;
     }
 }
